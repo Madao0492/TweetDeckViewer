37ece9740a5d3d0bbf29f4b04d6b6ae5ff6d55f5
Madao0492 <madao_mk@yahoo.co.jp>
2016-12-16 17:51:31 +0900

    first commit

---
+++ TweetDeckViewer/Main.Designer.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows.Forms;
+
+namespace TweetDeckViewer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.ScrollBarsEnabled = false;
+            this.webBrowser1.Size = new System.Drawing.Size(284, 261);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.Url = new System.Uri("https://tweetdeck.twitter.com/", System.UriKind.Absolute);
+            this.webBrowser1.NewWindow += new System.ComponentModel.CancelEventHandler(this.webBrowser1_NewWindow);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Main";
+            this.Text = "TweetDeckViewer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ TweetDeckViewer/Main.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TweetDeckViewer
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            this.Icon = new Icon(@"icon.ico");
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            // ウィンドウの位置・サイズを復元
+            Bounds = Properties.Settings.Default.Bounds;
+            WindowState = Properties.Settings.Default.WindowState;
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // ウィンドウの位置・サイズを保存
+            if (WindowState == FormWindowState.Normal)
+                Properties.Settings.Default.Bounds = Bounds;
+            else
+                Properties.Settings.Default.Bounds = RestoreBounds;
+
+            Properties.Settings.Default.WindowState = WindowState;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void webBrowser1_NewWindow(object sender, CancelEventArgs e)
+        {
+            WebBrowser wb = (WebBrowser)sender;
+            string txt = wb.StatusText;
+            if (txt != "")
+            {
+                System.Diagnostics.Process.Start(txt);
+            }
+            //ここで標準ブラウザで開くのをキャンセルしている
+            e.Cancel = true;
+        }
+    }
+}
